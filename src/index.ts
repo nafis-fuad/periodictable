cd15d48e850ff193d2223e6bb8c414a3f34ae8cd
Nafis Fuad <fuadbd14@gmail.com>
2018-07-17 00:52:31 +0600

    adds required directories in src

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,10 @@ import express from "express";
 
 const app = express();
 
-app.get()
+app.get("/", (req, res) => {
+    res.send("<h1>hello world</h1>")
+});
+
+app.listen(3000, () => {
+    console.log("server's running on localhost:3000")
+});
