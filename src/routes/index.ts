b3c3661905ec78b74ad2bc49f977d6daba17ef91
Nafis Fuad <fuadbd14@gmail.com>
2018-07-21 15:17:26 +0600

    adds logic for identifying uniqueblocks and thier respctive colors

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,25 @@
 import { Request, Response } from "express";
+import * as pt from "periodic-table";
+
+// create an array with uniquegroupblocks
+const allEelements: object[] = pt.all();
+const allGroupBlocks: string[] = [];
+let uniqueGroupBlocks: string[];
+const unique = (value: string, index: number, self: any[]) => {
+    return self.indexOf(value) === index;
+}
+allEelements.forEach((element:any) => {
+    allGroupBlocks.push(element.groupBlock);
+});
+uniqueGroupBlocks = allGroupBlocks.filter(unique);
+
+
+// create required colors object
+const arrayOfColors = ["red", "green", "blue", "white", "black", "purple", "cyan", "rebeccapurple", "pink", "brown", "darkgreen"]
+const requiredColors:any = {};
+uniqueGroupBlocks.forEach((element:any, index:any) => {
+    requiredColors[element] = arrayOfColors[index];
+});
 
 /**
  * GET /
@@ -6,6 +27,8 @@ import { Request, Response } from "express";
  */
 export let index = (req: Request, res: Response) => {
     res.render("index", {
-        title: "Home"
+        title: "Home",
+        elements: pt.all(),
+        requiredColors,
     });
 };
