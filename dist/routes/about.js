d0e4d60d6a4805b1cd8c13a90c7f764ed2a1ca53
Nafis Fuad <fuadbd14@gmail.com>
2018-07-21 18:01:18 +0600

    deletes dist folder

--- dist/routes/about.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.about = function (req, res) {
-    res.render("about", {
-        title: "About"
-    });
-};
-//# sourceMappingURL=about.js.map
