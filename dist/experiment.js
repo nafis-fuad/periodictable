784fbc9c1746a5825f6a095a0a57024e63bce966
Nafis Fuad <fuadbd14@gmail.com>
2018-07-21 15:18:09 +0600

    renames partials to includes

--- dist/experiment.js
+++ dist/experiment.js
@@ -1,13 +1,2 @@
 "use strict";
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var pt = __importStar(require("periodic-table"));
-var elements = pt.all();
-console.log(elements);
 //# sourceMappingURL=experiment.js.map
--- dist/routes/index.js
+++ dist/routes/index.js
@@ -1,8 +1,33 @@
 "use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+var pt = __importStar(require("periodic-table"));
+var allEelements = pt.all();
+var allGroupBlocks = [];
+var uniqueGroupBlocks;
+var unique = function (value, index, self) {
+    return self.indexOf(value) === index;
+};
+allEelements.forEach(function (element) {
+    allGroupBlocks.push(element.groupBlock);
+});
+uniqueGroupBlocks = allGroupBlocks.filter(unique);
+var arrayOfColors = ["red", "green", "blue", "white", "black", "purple", "cyan", "rebeccapurple", "pink", "brown", "darkgreen"];
+var requiredColors = {};
+uniqueGroupBlocks.forEach(function (element, index) {
+    requiredColors[element] = arrayOfColors[index];
+});
 exports.index = function (req, res) {
     res.render("index", {
-        title: "Home"
+        title: "Home",
+        elements: pt.all(),
+        requiredColors: requiredColors,
     });
 };
 //# sourceMappingURL=index.js.map
