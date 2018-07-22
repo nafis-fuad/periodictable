aa6085d82955fad852f131e42e1f3a8728d13ac6
Nafis Fuad <fuadbd14@gmail.com>
2018-07-22 22:15:19 +0600

    adds client side js

---
+++ src/public/js/index.ts
@@ -0,0 +1,18 @@
+import MicroModal from "micromodal";
+
+MicroModal.init({
+    onShow: (modal: any) => console.info(`${modal.id} is shown`),
+    onClose: (modal: any) => console.info(`${modal.id} is hidden`),
+    openTrigger: "data-custom-open",
+    closeTrigger: "data-custom-close",
+    disableScroll: true,
+    disableFocus: false,
+    awaitCloseAnimation: true,
+    debugMode: true
+});
+
+
+const button = document.getElementById("button");
+button.addEventListener("click", (event) => {
+    MicroModal.show("modal-2", {});
+});
