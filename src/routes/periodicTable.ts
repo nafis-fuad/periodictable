c4c4de1e531fc6829bf1bde2cce87dcee457a9f5
Nafis Fuad <fuadbd14@gmail.com>
2018-08-03 00:09:46 +0600

    adds routes for periodic table

---
+++ src/routes/periodicTable.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import * as Pt from "periodic-table";
+
+// create an array with uniquegroupblocks
+const allEelements: object[] = Pt.all();
+const allGroupBlocks: string[] = [];
+let uniqueGroupBlocks: string[];
+const unique = (value: string, index: number, self: any[]) => {
+    return self.indexOf(value) === index;
+};
+allEelements.forEach((element: any) => {
+    allGroupBlocks.push(element.groupBlock);
+});
+uniqueGroupBlocks = allGroupBlocks.filter(unique);
+
+
+// create required colors object
+const arrayOfColors = ["#FAAF92", "#D4C7BE", "#AA6288", "#8abf7c;", "#89B4C4", "#F1D3A1", "#74ccb2", "#FDBC5E", "#553973", "#E04386", "#F7801A"];
+const requiredColors: any = {};
+uniqueGroupBlocks.forEach((element: any, index: any) => {
+    requiredColors[element] = arrayOfColors[index];
+});
+
+/**
+ * GET /
+ * PERIODIC TABLE PAGE.
+ */
+export let periodicTable = (req: Request, res: Response) => {
+    res.render("periodicTable", {
+        title: "Periodic Table",
+        elements: Pt.all(),
+        requiredColors,
+    });
+};
