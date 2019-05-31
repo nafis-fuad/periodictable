ec603990c176c78bc53e185e81a6e070b5d731c7
Nafis Fuad <fuadbd14@gmail.com>
2019-06-01 00:31:21 +0600

    Remove other routes Only keep the periodic table route as index
    changes the whole app functions

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,7 @@ import helmet from "helmet";
 dotenv.config({ path: ".env" });
 
 // Controllers (route handlers)
-import * as aboutController from "./routes/about";
 import * as homeController from "./routes/index";
-import * as periodicTableController from "./routes/periodicTable";
-import * as experimentController from "./routes/experiments";
 
 // Create Express server
 const app = express();
@@ -24,16 +21,13 @@ app.set("view engine", "pug");
 app.use(compression());
 
 app.use(
-    // express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
-    express.static(path.join(__dirname, "public"), { maxAge: 0 })
+  // express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
+  express.static(path.join(__dirname, "public"), { maxAge: 0 })
 );
 
 /**
  * Primary app routes.
  */
 app.get("/", homeController.index);
-app.get("/about", aboutController.about);
-app.get("/periodicTable", periodicTableController.periodicTable);
-app.get("/experiment", experimentController.experiment);
 
 export default app;
